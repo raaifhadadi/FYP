83e5e39cb46f03db1309e5dd3c965a4b6463e291
Raaif <hraaif@yahoo.com>
2024-05-22 15:53:09 +0100

    Add names to model layers for loading

--- models/cnn.py
+++ models/cnn.py
@@ -5,15 +5,15 @@ from tensorflow.keras import metrics
 def model(X, num_classes=5, filters = [12, 18, 24], kernels = [5,5,3], hidden_units=[128, 64], dropout=[0.5, 0.1]):
     
       
-    X = keras.layers.Conv1D(filters=filters[0], kernel_size=kernels[0], input_shape=X.shape[1:])(X)
+    X = keras.layers.Conv1D(filters=filters[0], kernel_size=kernels[0], input_shape=X.shape[1:], name='conv1')(X)
     
     for i in range(len(filters)-1):
-        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.BatchNormalization(name=f'bn{i+1}')(X)
         X = keras.layers.Activation('leaky_relu')(X)
         X = keras.layers.MaxPooling1D(2)(X)
-        X = keras.layers.Conv1D(filters=filters[i+1], kernel_size=kernels[i+1])(X)
+        X = keras.layers.Conv1D(filters=filters[i+1], kernel_size=kernels[i+1], name=f'conv{i+2}')(X)
         
-    X = keras.layers.BatchNormalization()(X)
+    X = keras.layers.BatchNormalization(name='bn_final')(X)
     X = keras.layers.Activation('leaky_relu')(X)
     
     features_output = keras.layers.GlobalAveragePooling1D()(X)
@@ -23,8 +23,8 @@ def model(X, num_classes=5, filters = [12, 18, 24], kernels = [5,5,3], hidden_un
     
     for i in range(len(hidden_units)):
         X = keras.layers.Dropout(dropout[i])(X)
-        X = keras.layers.Dense(hidden_units[i], activation='leaky_relu')(X)
+        X = keras.layers.Dense(hidden_units[i], activation='leaky_relu', name=f'dense{i+1}')(X)
         
-    X = keras.layers.Dense(num_classes, activation='sigmoid')(X)
+    X = keras.layers.Dense(num_classes, activation='sigmoid', name='output')(X)
     
     return X, features_output
--- models/resnet.py
+++ models/resnet.py
@@ -2,32 +2,32 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import metrics
 
-def resnet_block(X, filters, kernel_size=3):
+def resnet_block(X, filters, kernels=[5, 3], index=0):
         
         X_shortcut = X
         
-        X = keras.layers.Conv1D(filters=filters[0], kernel_size=kernel_size, padding='same')(X)
-        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.Conv1D(filters=filters[0], kernel_size=kernels[0], padding='same', name=f'conv_{index}_1')(X)
+        X = keras.layers.BatchNormalization(name=f'bn_{index}_1')(X)
         X = keras.layers.Activation('relu')(X)
         
-        X = keras.layers.Conv1D(filters=filters[1], kernel_size=kernel_size, padding='same')(X)
-        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.Conv1D(filters=filters[1], kernel_size=kernels[1], padding='same', name=f'conv_{index}_2')(X)
+        X = keras.layers.BatchNormalization(name=f'bn_{index}_2')(X)
         
         X = keras.layers.concatenate([X, X_shortcut])
         X = keras.layers.Activation('relu')(X)
         
         return X
     
-def model(X, num_classes=5, filters = [16, 16], kernels = 5, layers=4, hidden_units=128):
+def model(X, num_classes=5, filters = [16, 16], kernels = [5, 3], layers=4, hidden_units=128):
         
-        X = keras.layers.Conv1D(filters=filters[0], kernel_size=kernels, input_shape=X.shape[1:])(X)
-        X = keras.layers.BatchNormalization()(X)
+        X = keras.layers.Conv1D(filters=filters[0], kernel_size=kernels[0], input_shape=X.shape[1:], name='conv1')(X)
+        X = keras.layers.BatchNormalization(name='bn1')(X)
         X = keras.layers.Activation('leaky_relu')(X)
         
         X = keras.layers.MaxPooling1D(2)(X)
         
         for i in range(layers):
-            X = resnet_block(X, filters, kernel_size=kernels)
+            X = resnet_block(X, filters, kernels=kernels, index=i+1)
             
             if i == 1:
                 X = keras.layers.MaxPooling1D(2)(X)
@@ -35,9 +35,9 @@ def model(X, num_classes=5, filters = [16, 16], kernels = 5, layers=4, hidden_un
         features_output = keras.layers.GlobalAveragePooling1D()(X)
         X = keras.layers.Dropout(0.5)(features_output)
         
-        X = keras.layers.Dense(hidden_units, activation='leaky_relu')(X)
+        X = keras.layers.Dense(hidden_units, activation='leaky_relu', name='dense1')(X)
         X = keras.layers.Dropout(0.1)(X)
-        X = keras.layers.Dense(hidden_units/2, activation='leaky_relu')(X)
-        X = keras.layers.Dense(num_classes, activation='sigmoid')(X)
+        X = keras.layers.Dense(hidden_units/2, activation='leaky_relu', name='dense2')(X)
+        X = keras.layers.Dense(num_classes, activation='sigmoid', name='output')(X)
         
         return X, features_output
